77c7a6a586fb0b38ff106d397b4c3708353db63d
Josimara <josybehlul@gmail.com>
2022-01-19 15:05:14 -0300

    modal para visualizacao de uma foto e suas informaçoes

--- src/components/Feed/FeedModal.js
+++ src/components/Feed/FeedModal.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { PHOTO_GET } from '../../api'
+import useFetch from '../../hooks/useFetch'
+import Error from '../Helper/Error'
+import Loading from '../Helper/Loading'
+import PhotoContent from '../Photo/PhotoContent'
+import styles from './FeedModal.module.css'
 
-const FeedModal = () => {
+const FeedModal = ({photo, setModalPhoto}) => {
+  const {data, error, loading, request} = useFetch()
+
+  React.useEffect(() => {
+    const {url, options} = PHOTO_GET(photo.id)
+   request(url, options)
+  },[photo, request])
+
+  function handleOutsideClick(event){
+    if(event.target === event.currentTarget){
+      setModalPhoto(null)
+    }
+  }
   return (
-    <div>
-      jjjj
+    <div className={styles.modal} onClick={handleOutsideClick}> 
+      
+      {error && <Error error={error} />}
+      {loading && <Loading/>}
+      {data && <PhotoContent data={data}/>}
     </div>
   )
 }
--- src/components/Feed/FeedPhotoItem.js
+++ src/components/Feed/FeedPhotoItem.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import styles from './FeedPhotoItem.module.css'
 
-const FeedPhotoItem = ({photo}) => {
-  return <li className={styles.photo}>
+const FeedPhotoItem = ({photo, setModalPhoto}) => {
+
+  function handleClick(){
+    setModalPhoto(photo)
+    console.log(photo)
+  }
+  return <li className={styles.photo} onClick={handleClick}>
           <img src={photo.src} alt={photo.title}></img>
           <span className={styles.visualizacao}>{photo.acessos}</span>
         </li>
