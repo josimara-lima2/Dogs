1315409c8e681552c0a73c301f1eeaf8c0a4c82b
Josimara <josybehlul@gmail.com>
2022-01-20 15:09:14 -0300

    add button delete de post

--- src/api.js
+++ src/api.js
@@ -1,107 +1,110 @@
-export const API_URL = 'https://dogsapi.origamid.dev/json/'
+export const API_URL = "https://dogsapi.origamid.dev/json/";
 
-export function TOKEN_POST(body){
-  return{
-    url: API_URL+'jwt-auth/v1/token',
-    options:{
-      method: 'POST',
+export function TOKEN_POST(body) {
+  return {
+    url: API_URL + "jwt-auth/v1/token",
+    options: {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-     body: JSON.stringify(body),
-    }
-  }
+      body: JSON.stringify(body),
+    },
+  };
 }
 
-export function TOKEN_VALIDATE_POST(token){
-  return{
-    url: API_URL+'jwt-auth/v1/token/validate',
-    options:{
-      method: 'POST',
+export function TOKEN_VALIDATE_POST(token) {
+  return {
+    url: API_URL + "jwt-auth/v1/token/validate",
+    options: {
+      method: "POST",
       headers: {
-        Authorization:'Bearer '+ token,
+        Authorization: "Bearer " + token,
       },
-    
-    }
-  }
+    },
+  };
 }
 
-
-export function USER_GET(token){
-  return{
-    url: API_URL +'api/user',
-    options:{
-      method: 'GET',
+export function USER_GET(token) {
+  return {
+    url: API_URL + "api/user",
+    options: {
+      method: "GET",
       headers: {
-        Authorization:'Bearer '+ token,
+        Authorization: "Bearer " + token,
       },
-     
-    }
-  }
+    },
+  };
 }
 
-export function USER_POST(body){
-  return{
-    url: API_URL +'api/user',
-    options:{
-      method: 'POST',
+export function USER_POST(body) {
+  return {
+    url: API_URL + "api/user",
+    options: {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-     body: JSON.stringify(body)
-    }
-  }
+      body: JSON.stringify(body),
+    },
+  };
 }
 
-export function PHOTO_POST(token,formData){
-  return{
-    url: API_URL +'api/photo',
-    options:{
-      method: 'POST',
+export function PHOTO_POST(token, formData) {
+  return {
+    url: API_URL + "api/photo",
+    options: {
+      method: "POST",
       headers: {
-        Authorization:'Bearer '+ token,
+        Authorization: "Bearer " + token,
       },
-      body: formData
-     
-    }
-  }
+      body: formData,
+    },
+  };
 }
 
-export function PHOTOS_GET({page, total, user}){
-  return{
+export function PHOTOS_GET({ page, total, user }) {
+  return {
     url: `${API_URL}api/photo/?_page=${page}&_total=${total}&_user=${user}`,
-    options:{
-      method: 'GET',
-      cache: 'no-store'
-      
-    }
-  }
+    options: {
+      method: "GET",
+      cache: "no-store",
+    },
+  };
 }
 
-export function PHOTO_GET(id){
-  return{
+export function PHOTO_GET(id) {
+  return {
     url: `${API_URL}api/photo/${id}`,
-    options:{
-      method: 'GET',
-      cache: 'no-store'
-      
-    }
-  }
+    options: {
+      method: "GET",
+      cache: "no-store",
+    },
+  };
 }
 
-export function  COMMENT_POST(id,body,token) {
+export function COMMENT_POST(id, body, token) {
   return {
     url: `${API_URL}api/comment/${id}`,
-    options:{
-      method: 'POST',
-     headers: {
-      'Content-Type': 'application/json',
-      Authorization: 'Bearer ' + token,
+    options: {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      body: JSON.stringify(body),
     },
-    body: JSON.stringify(body)
-  
-    }
-  }
+  };
 }
- 
-  
+
+export function PHOTO_DELETE(id) {
+  return {
+    url: `${API_URL}api/photo/${id}`,
+    options: {
+      method: "DELETE",
+      headers: {
+        Authorization: "Bearer " + window.localStorage.getItem("token"),
+      },
+    },
+  };
+}
