d2931ddcdf4159993f9877c93c75ea7426dbebc1
Josimara <josybehlul@gmail.com>
2022-01-20 15:28:34 -0300

    add skeleton de imagens

--- src/components/Feed/FeedPhotoItem.js
+++ src/components/Feed/FeedPhotoItem.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import styles from './FeedPhotoItem.module.css'
+import React from "react";
+import Image from "../Helper/Image";
+import styles from "./FeedPhotoItem.module.css";
 
-const FeedPhotoItem = ({photo, setModalPhoto}) => {
-
-  function handleClick(){
-    setModalPhoto(photo)
-    console.log(photo)
+const FeedPhotoItem = ({ photo, setModalPhoto }) => {
+  function handleClick() {
+    setModalPhoto(photo);
+    console.log(photo);
   }
-  return <li className={styles.photo} onClick={handleClick}>
-          <img src={photo.src} alt={photo.title}></img>
-          <span className={styles.visualizacao}>{photo.acessos}</span>
-        </li>
-}
+  return (
+    <li className={styles.photo} onClick={handleClick}>
+      <Image src={photo.src} alt={photo.title} />
 
-export default FeedPhotoItem
+      <span className={styles.visualizacao}>{photo.acessos}</span>
+    </li>
+  );
+};
+
+export default FeedPhotoItem;
